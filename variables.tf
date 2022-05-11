60202379891cc4bea5b0a953a12139c538545bff
Dawson <ddaws@users.noreply.github.com>
2022-05-11 19:56:09 -0300

    Added remaining session manager options

--- variables.tf
+++ variables.tf
@@ -35,3 +35,32 @@ variable "cloudwatch_streaming_enabled" {
   default     = true
   description = "(Optional) Stream session log data to CloudWatch. Defaults to true. If false logs will be uploaded at the end of the session."
 }
+
+variable "idle_session_timeout" {
+  type        = number
+  default     = 20
+  description = "(Optional) Time until a session is closed when left idle."
+}
+
+variable "max_session_duration" {
+  type        = number
+  default     = null
+  description = "(Optional) The longest a session can stay open before it will be closed."
+}
+
+variable "run_as_enabled" {
+  type    = bool
+  default = false
+}
+
+variable "linux_shell_profile" {
+  type        = string
+  default     = ""
+  description = "(Optional) A set of Linux commands to run when a Linux session is started."
+}
+
+variable "windows_shell_profile" {
+  type        = string
+  default     = ""
+  description = "(Optional) A set of Windows commands to run when a Windows session is started."
+}
